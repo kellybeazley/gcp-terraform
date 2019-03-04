ed48afb2308457debca0911b72384b1066ca2172
Kelly.Beazley <kelly.beazley@springernature.com>
2019-03-04 15:15:57 +0000

    tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+#Configure GCP Provider
+provider "google" {
+  credentials = "${file("kelly-gcp-sa.json")}"
+  project     = "kelly-gcp"
+  region      = "europe-west1"
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ name         = "kelly-tf"
+ machine_type = "f1-micro"
+ zone = "europe-west1-b"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
